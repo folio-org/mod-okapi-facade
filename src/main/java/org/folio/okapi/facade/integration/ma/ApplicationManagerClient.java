70c25b5160e2e9b64f8186ca4537c7f43797ae38
Mykola Makhin <mmakhin@ebsco.com>
2024-08-23 12:30:25 +0300

    MODOKAPFAC-3: Removed unused Feign client methods

--- src/main/java/org/folio/okapi/facade/integration/ma/ApplicationManagerClient.java
+++ src/main/java/org/folio/okapi/facade/integration/ma/ApplicationManagerClient.java
@@ -4,30 +4,12 @@ import static org.folio.common.utils.OkapiHeaders.TOKEN;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import org.folio.okapi.facade.integration.ma.model.ApplicationDescriptor;
-import org.folio.okapi.facade.integration.ma.model.ModuleDiscovery;
 import org.folio.okapi.facade.integration.model.ResultList;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ApplicationManagerClient {
-
-  /**
-   * Retrieves {@link ApplicationDescriptor} value by id from mgr-applications.
-   *
-   * @param applicationId - application descriptor identifier
-   * @param full - defines if application descriptor must be loaded with all module descriptors
-   * @param token - okapi auth token
-   * @return retrieved {@link ApplicationDescriptor} object
-   */
-  @GetMapping(value = "/applications/{id}", consumes = APPLICATION_JSON_VALUE)
-  ApplicationDescriptor getApplicationDescriptor(
-    @PathVariable("id") String applicationId,
-    @RequestParam("full") Boolean full,
-    @RequestHeader(TOKEN) String token);
 
   @GetMapping(value = "/applications", consumes = APPLICATION_JSON_VALUE)
   ResultList<ApplicationDescriptor> queryApplicationDescriptors(
@@ -35,14 +17,5 @@ public interface ApplicationManagerClient {
     @RequestParam("full") Boolean full,
     @RequestParam("limit") int limit,
     @RequestParam("offset") int offset,
-    @RequestHeader(TOKEN) String token);
-
-  @PostMapping(value = "/applications/validate", consumes = APPLICATION_JSON_VALUE)
-  void validate(@RequestBody ApplicationDescriptor descriptor, @RequestHeader(TOKEN) String token);
-
-  @GetMapping(value = "/applications/{id}/discovery", consumes = APPLICATION_JSON_VALUE)
-  ResultList<ModuleDiscovery> getModuleDiscoveries(
-    @PathVariable("id") String applicationId,
-    @RequestParam("limit") int limit,
     @RequestHeader(TOKEN) String token);
 }
--- src/main/java/org/folio/okapi/facade/integration/tm/TenantManagerClient.java
+++ src/main/java/org/folio/okapi/facade/integration/tm/TenantManagerClient.java
@@ -3,7 +3,6 @@ package org.folio.okapi.facade.integration.tm;
 import static org.folio.common.utils.OkapiHeaders.TOKEN;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-import java.util.UUID;
 import org.folio.okapi.facade.integration.model.ResultList;
 import org.folio.okapi.facade.integration.tm.model.Tenant;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -13,17 +12,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "tenant", url = "${application.mt.url}", configuration = TenantManagerClientConfiguration.class)
 public interface TenantManagerClient {
-
-  /**
-   * Retrieves tenant by id from mgr-tenants.
-   *
-   * @param id - tenant identifier
-   * @param token - optional x-okapi-token header value for authorization in Okapi
-   * @return found {@link Tenant} object
-   */
-  @GetMapping(value = "/tenants/{id}", consumes = APPLICATION_JSON_VALUE)
-  Tenant getTenantById(@PathVariable UUID id, @RequestHeader(TOKEN) String token);
-
   /**
    * Queries tenant by name from mgr-tenants.
    *
