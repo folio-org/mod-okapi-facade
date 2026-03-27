0f8abd3c49b3e9665c706c0c92329e225dacdea3
Vavilkin <yauhen_vavilkin@epam.com>
2026-03-27 17:02:16 +0400

    MODOKAPFAC-23: exclude DataSourceAutoConfiguration in OkapiFacadeApplication and application.yml

--- src/main/java/org/folio/okapi/facade/OkapiFacadeApplication.java
+++ src/main/java/org/folio/okapi/facade/OkapiFacadeApplication.java
@@ -3,9 +3,10 @@ package org.folio.okapi.facade;
 import org.folio.common.configuration.properties.FolioEnvironment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @Import(FolioEnvironment.class)
 public class OkapiFacadeApplication {
 
